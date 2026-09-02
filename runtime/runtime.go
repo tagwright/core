dbc32396432ba10443dcaacb8d6b7a9224e1e81b
techgaud <tagwright@mail.techgaud.com>
2026-09-02 19:34:52 +0000

    legal: name techgaud as the copyright holder

--- runtime/runtime.go
+++ runtime/runtime.go
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) 2026 techgaud
 
 // Package runtime abstracts a container runtime (Docker or Podman) behind the
 // small set of operations Ballast needs: discover containers and their mounts,
